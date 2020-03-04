d89ed17c5e57d48bc85c3552e021249aee2aa6fb
Virginia Ladd <virginialadd2@gmail.com>
2020-03-04 10:31:31 -0700

    Got the urls displaying how I want and stored how I want

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -3,32 +3,35 @@ import './UrlContainer.css';
 import { connect } from 'react-redux';
 import { setUrls } from '../../actions';
 import { getUrls } from '../../apiCalls';
+let urlEls = [];
 
 class UrlContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      urls: []
+      urls: this.props.urls
     }
   }
 
   componentDidMount() {
     getUrls()
-      .then(data => this.props.setUrls(data.urls))
-      .then(data => this.setState({ urls: data.urls }))
-      .catch(err => console.error('Error fetching:', err));
+    .then(data => data.urls.forEach((url) => this.props.setUrls(url)))
+    .catch(err => console.error('Error fetching:', err))
   }
 
+
+
   render() {
-    const urlEls = this.state.urls.map(url => {
-     return (
-       <div className="url">
-         <h3>{url.title}</h3>
-         <a href={url.short_url} target="blank">{url.short_url}</a>
-         <p>{url.long_url}</p>
-       </div>
-     )
-   })
+     const urlEls = this.props.urls.map(url => {
+      console.log(url)
+       return (
+         <div className="url">
+           <h3>{url.title}</h3>
+           <a href={url.short_url} target="blank">{url.short_url}</a>
+           <p>{url.long_url}</p>
+         </div>
+       )
+     })
     return (
       <section>
       { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 // import { connect } from 'react-redux';
 // import { setUrls } from '../../actions';
-import { getUrls } from '../../apiCalls';
+// import { getUrls } from '../../apiCalls';
 import UrlContainer from '../../components/UrlContainer/UrlContainer';
 import UrlForm from '../../components/UrlForm/UrlForm';
 
